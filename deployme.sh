3a618d7ccbeeb5850ba473a57b6c1b8ca264e46b
manojkmgit <39760825+manojkmgit@users.noreply.github.com>
2021-12-03 19:33:21 +0530

    Update deployme.sh

--- deployme.sh
+++ deployme.sh
@@ -9,7 +9,7 @@ environment_name="Wishlistapp-env"
 NAME=user-wishlist-app
 EB_BUCKET=jenkins-docker-aws-eb
 
-VERSION="v3"
+VERSION="main"
 ZIP="code.zip"
 
 aws configure set default.region ap-south-1
