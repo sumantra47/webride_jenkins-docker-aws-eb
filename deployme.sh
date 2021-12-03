23cc6a32d7e7737036f6f177ef348d59a06696f2
manojkmgit <39760825+manojkmgit@users.noreply.github.com>
2021-12-03 19:51:08 +0530

    Update deployme.sh

--- deployme.sh
+++ deployme.sh
@@ -17,12 +17,15 @@ aws configure set default.region ap-south-1
 # Zip up the Dockerrun file (feel free to zip up an .ebextensions directory with it)
 zip -r $ZIP Dockerrun.aws.json
 
+echo "Copying new zip to S3"
 aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
 
 # Create a new application version with the zipped up Dockerrun file
+echo "Creating new application version"
 aws elasticbeanstalk create-application-version --application-name $application_name \
     --version-label $VERSION --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
 
 # Update the environment to use the new application version
+echo "Updating environment"
 aws elasticbeanstalk update-environment --environment-name $environment_name \
       --version-label $VERSION
